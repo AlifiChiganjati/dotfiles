de82d44b8fcd641611cda509291abab05fd2d9d7
Alifi Chiganjati <alifichiganjati@gmail.com>
2024-10-12 08:13:41 +0700

    REFACTOR: change lockscreen and suspend for xautolock script

--- scripts/xautolock.sh
+++ scripts/xautolock.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# Mengunci layar dengan betterlockscreen setelah 1 menit
-xautolock -time 5 -locker "betterlockscreen -l" &
+xautolock -detectsleep -time 5 -locker "betterlockscreen -l" &
 
-# Suspend setelah 2 menit
-(sleep 600 && loginctl suspend) &
+FIRST_XAUTLOCK_PID=$!
+
+(xautolock -detectsleep -time 7 -locker "loginctl suspend" &)
+
+wait $FIRST_XAUTLOCK_PID
