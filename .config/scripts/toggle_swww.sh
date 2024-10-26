04c059ea996e7c5ad1a4135b7722a4a373ab6d64
Alifi Chiganjati <alifichiganjati@gmail.com>
2024-10-27 01:21:16 +0700

    delete: debug echo for toggle swww-daemon

--- .config/scripts/toggle_swww.sh
+++ .config/scripts/toggle_swww.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if pgrep -x "swww-daemon" >/dev/null; then
-	echo "swww-daemon sedang berjalan. Mematikan..."
 	killall swww-daemon
 else
-	echo "swww-daemon tidak berjalan. Menyalakan..."
 	swww-daemon &
 fi
