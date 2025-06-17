b6a63bf77b9e08891cda455a9f449862b911b790
Alifi Chiganjati <alifichiganjati@gmail.com>
2025-06-16 23:39:25 -0700

    FIX: start for bspwm

--- start/start-bspwm.sh
+++ start/start-bspwm.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
+# Set XDG_RUNTIME_DIR if not set
 if [ -z "${XDG_RUNTIME_DIR}" ]; then
-  # Set XDG_RUNTIME_DIR to /tmp/<UID>-runtime-dir, using $(id -u) in case $UID is not set
   export XDG_RUNTIME_DIR="/tmp/$(id -u)-runtime-dir"
-
-  # Create the directory if it doesn't exist and set appropriate permissions
-  if [ ! -d "${XDG_RUNTIME_DIR}" ]; then
-    mkdir -p "${XDG_RUNTIME_DIR}"
-    chmod 0700 "${XDG_RUNTIME_DIR}"
-  fi
+  [ ! -d "${XDG_RUNTIME_DIR}" ] && mkdir -p "${XDG_RUNTIME_DIR}" && chmod 0700 "${XDG_RUNTIME_DIR}"
 fi
 
 dbus-run-session bspwm
