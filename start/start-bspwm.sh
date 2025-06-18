64309bfcfb5eff2e1896a2f1766c803e9c14863a
Alifi Chiganjati <alifichiganjati@gmail.com>
2025-06-17 18:56:01 -0700

    FEAT: change start be the default settings

--- start/start-bspwm.sh
+++ start/start-bspwm.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
-# Set XDG_RUNTIME_DIR if not set
-if [ -z "${XDG_RUNTIME_DIR}" ]; then
-  export XDG_RUNTIME_DIR="/tmp/$(id -u)-runtime-dir"
-  [ ! -d "${XDG_RUNTIME_DIR}" ] && mkdir -p "${XDG_RUNTIME_DIR}" && chmod 0700 "${XDG_RUNTIME_DIR}"
+if test -z "${XDG_RUNTIME_DIR}"; then
+  export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
+  if ! test -d "${XDG_RUNTIME_DIR}"; then
+    mkdir "${XDG_RUNTIME_DIR}"
+    chmod 0700 "${XDG_RUNTIME_DIR}"
+  fi
 fi
 
 dbus-run-session bspwm
--- start/start-hyprland.sh
+++ start/start-hyprland.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-
-if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-
-  # Set XDG_RUNTIME_DIR jika belum ada
-  if [ -z "${XDG_RUNTIME_DIR}" ]; then
-    export XDG_RUNTIME_DIR="/tmp/$(id -u)-runtime-dir"
-    [ ! -d "${XDG_RUNTIME_DIR}" ] && mkdir -p "${XDG_RUNTIME_DIR}" && chmod 0700 "${XDG_RUNTIME_DIR}"
+if test -z "${XDG_RUNTIME_DIR}"; then
+  export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
+  if ! test -d "${XDG_RUNTIME_DIR}"; then
+    mkdir "${XDG_RUNTIME_DIR}"
+    chmod 0700 "${XDG_RUNTIME_DIR}"
   fi
-
-  # Bersihkan wayland socket lama
-  rm -f "${XDG_RUNTIME_DIR}/wayland-"*
-
-  # Pastikan socket Wayland pakai wayland-0
-  export WAYLAND_DISPLAY=wayland-0
-
-  # Jalankan Hyprland via seatd-launch dan dbus-run-session
-  exec dbus-run-session seatd-launch Hyprland
 fi
+
+dbus-run-session Hyprland
