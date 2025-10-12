6d4a36db81f1256064f4dbc09fcab109cb7429bd
Alifi Chiganjati <alifichiganjati@gmail.com>
2025-10-12 21:51:53 +0800

    FEAT: xdg-portal

--- config/scripts/xdg-portal.sh
+++ config/scripts/xdg-portal.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+# Tunggu sedikit supaya sesi Hyprland benar-benar siap
 sleep 1
+
+# Matikan semua portal lama
 killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gtk
 killall xdg-desktop-portal
+
+# Jalankan ulang portal Hyprland
 /usr/libexec/xdg-desktop-portal-hyprland &
+
+# Tunggu agar Hyprland portal siap sebelum start portal GTK
 sleep 2
+
+# Jalankan portal GTK
+/usr/libexec/xdg-desktop-portal-gtk &
+
+# Tunggu sedikit sebelum menjalankan portal utama
+sleep 2
+
+# Jalankan portal utama
 /usr/libexec/xdg-desktop-portal &
